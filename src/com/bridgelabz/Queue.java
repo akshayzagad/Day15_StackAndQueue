4b488267dc1f2b6f981158efc3fb43c47967eced
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-24 20:37:38 +0530

    Added Creat Queue Append And Display Element in Queue

---
+++ src/com/bridgelabz/Queue.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class Queue {
+    /*
+     * creating Object
+     */
+    LinkedList linkedList = new LinkedList();
+
+    /*
+     * Appending the value at first place calling method through object
+     */
+    public void append(Object data) {
+        linkedList.append(data);
+    }
+    /*
+     * calling method to display value in linkedList/stack
+     */
+    public void displayQueue() {
+        linkedList.show();
+    }
+}
---
+++ src/com/bridgelabz/QueueMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Queue programme");
+        Queue myQueue = new Queue();
+        System.out.println("Add data to create stack");
+        myQueue.append(56);
+        myQueue.append(30);
+        myQueue.append(56);
+        System.out.println("show data according fifo rule stack");
+        myQueue.displayQueue();
+    }
+}
